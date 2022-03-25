04cf0fdb38ceced9f7cc5a0bdc42df7ec29d964f
wiserain <mail275@gmail.com>
2022-03-26 08:02:53 +0900

    separate Dockerfile for latest tags

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+
+if [ $# -eq 0 ]; then echo "Usage: $0 <context>"; exit 1; fi
 
 name="compile_requirements"
 
@@ -12,6 +15,6 @@ if [ $(docker images -q $name | wc -l) -ne 1 ]; then
 fi
 
 cid=$(docker ps -a | grep $name | awk '{print $1}')
-docker run --rm -i -v ${PWD}:/req $name \
+docker run --rm -i -v ${PWD}/$1:/req $name \
     bash -c "cd /req && pip-compile -U"
-sudo chown $(id -u):$(id -g) requirements.*
+sudo chown $(id -u):$(id -g) $1/requirements.*
